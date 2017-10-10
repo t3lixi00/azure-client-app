d1893d47f423203006700dd0bc2ff347ff19dde1
Rita <t3lixi00@students.oamk.fi>
2017-10-10 15:01:24 +0300

    add scope

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ $(document).ready(function(){
     var token;
     var client_id = "938228a1-9ac9-4084-8ede-3c9088ba476a";
     var clientSecret = "06fR2q/onGGgW3pgLxGrlsTE+DnG+fRLpFCc9Dk/YRw=";
-    var tokenurl = "https://login.microsoftonline.com/common/oauth2/v2.0/token"; 
+    var tenant_id = `ea14b53f-1ff1-4682-92a4-b59d20531b04`;
+    var tokenurl = `https://login.microsoftonline.com/${tenant_id}/oauth2/token"`;
+    var scope= "https://outlook.office.com/mail.read"; 
     var request = new XMLHttpRequest();
 
- function getToken(tokenurl, client_id, clientSecret ){
+ function getToken(tenant_id, tokenurl, client_id, clientSecret, scope){
      var key;
      request.open("POST", tokenurl, true );
      request.setRequestHeader("Content-type", "application/json");
-     request.send("grant_type=client_credentials&client_id=" + client_id+"&"+"client_secret="+clientSecret);
+     request.send("grant_type=client_credentials&client_id=" + client_id+"&"+"client_secret="+clientSecret+ "&"+"scope="+scope);
      request.onreadystatechange = function(){
          if(request.readyState == request.DONE){
              var response = request.responseText;
