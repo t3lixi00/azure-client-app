0199eda9a78ad1eae707017ecec592ca5997ba5a
Rita <t3lixi00@students.oamk.fi>
2017-10-10 16:48:33 +0300

    change header

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ console.log("working!");
 
 $(document).ready(function(){
 
-    var token;
+  /*  var token;
     var client_id = "938228a1-9ac9-4084-8ede-3c9088ba476a";
     var clientSecret = "06fR2q/onGGgW3pgLxGrlsTE+DnG+fRLpFCc9Dk/YRw=";
     var tenant_id = `ea14b53f-1ff1-4682-92a4-b59d20531b04`;
@@ -34,7 +34,7 @@ $(document).ready(function(){
 
  }
 
-getToken(token, client_id, clientSecret);
+getToken(token, client_id, clientSecret);*/
 
     $(".button").click(function(){
        $.ajax({
@@ -42,7 +42,8 @@ getToken(token, client_id, clientSecret);
            url:"https://todolist2.azurewebsites.net/tasks",
            headers:{
                 
-                'Content-Type':'application/x-www-form-urlencoded'
+                'Content-Type':'application/x-www-form-urlencoded',
+                'Access-Control-Allow-Origin':'*'
            },
            method:"GET",
            success:function(data){
