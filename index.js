5ed1027c548a235577efb9daf66bb684499c2b4a
Rita <t3lixi00@students.oamk.fi>
2017-10-11 22:18:49 +0300

    change responsebody2

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
      request.send("grant_type=client_credentials&client_id=" + client_id+"&"+"client_secret="+clientSecret+ "&"+"resource="+resource);
      request.onreadystatechange = function(){
          if(request.readyState == request.DONE){
-           var obj = JSON.parse(responseText);
+           var obj = JSON.parse(request.responseText);
              token = obj.access_token; 
              console.log(token);
            //  console.log(request.responseText);
