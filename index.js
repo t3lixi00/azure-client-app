60ed0f7549e09b436a18e823ba29eb0e329e3947
Rita <t3lixi00@students.oamk.fi>
2017-10-11 22:34:55 +0300

    add show responseText

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ $(document).ready(function(){
      request.send("grant_type=client_credentials&client_id=" + client_id+"&"+"client_secret="+clientSecret+ "&"+"resource="+resource);
      request.onreadystatechange = function(){
          if(request.readyState == request.DONE){
+            document.getElementsByClassName("well").innerHTML = request.responseText;
            var obj = JSON.parse(this.responseText);
              token = obj.access_token; 
              console.log(token);
